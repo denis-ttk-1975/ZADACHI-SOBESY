f848c9c288f79f2586a1663af779483cb0d38022
denis-ttk-1975 <denis.ttk@gmail.com>
2023-06-16 15:33:15 +0300

    errors fixed

--- IBS/src/App.tsx
+++ IBS/src/App.tsx
@@ -1,25 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Input = () => {
-  const [valueInput, setValue] = useState("");
+  const [valueInput, setValue] = useState('');
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   return (
     <>
-      <input
-        value={valueInput}
-        onChange={(e) => onChangeInput(e)}
-        name="value"
-      />
+      <input value={valueInput} onChange={onChangeInput} name='value' />
       <div>valueInput = {valueInput}</div>
     </>
   );
 };
 
 export const App = () => (
-  <div className="App">
+  <div className='App'>
     <Input />
   </div>
 );
